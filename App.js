5b46650bf65c8c81c71ba586496acc6e9c53c1ef
taehoon <https://github.com/allstar94>
2020-12-25 02:51:04 +0900

    finish

--- App.js
+++ App.js
@@ -1,49 +1,53 @@
-import {StatusBar} from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Alert} from "react-native";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 import axios from "axios";
 import Weather from './Weather';
 
-const API_KEY ="a4ebc913ef8067572276874162bbab8b";
+const API_KEY = "a4ebc913ef8067572276874162bbab8b";
 
 export default class extends React.Component {
   state = {
-    isLoading:true
+    isLoading: true
   };
-  getWeather = async(latitude, longitude) =>{
-    const{
+  getWeather = async (latitude, longitude) => {
+    const {
       data: {
-        main:{temp},
+        main: { temp },
         weather
       }
     } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
+    
     this.setState({
-      isLoading:false,
+      isLoading: false,
       condition: weather[0].main,
       temp
-    });  
+    });
+    console.log(weather);
   };
-  getLocation = async() => {
-    try{
+  getLocation = async () => {
+    try {
       await Location.requestPermissionsAsync();
       const {
-        coords:{latitude, longitude}
+        coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude)
       // this.setState({isLoading:false}); 
-    }catch(error){
-        Alert.alert("can't find you.", "so sad");
-    }  
+    } catch (error) {
+      Alert.alert("can't find you.", "so sad");
+    }
   };
-  componentDidMount(){
-    this.getLocation(); 
+
+  componentDidMount() {
+    this.getLocation();
+    
   }
-  render(){
-    const{ isLoading, temp, condition } = this.state;
-    return isLoading?<Loading/> : <Weather temp={Math.round(temp)} condition={condition} />;
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,25 +1,131 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native"; 
-import PropTypes from "prop-types";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import propTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from 'expo-linear-gradient';
+
+const weatherOption = {
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Haze",
+        subtitle: "Jst don't go outside."
+    },
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#007ADF"],
+        title: "",
+        subtitle: ""
+    },
+    Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89E7FE", "#66A6FF"],
+        title: "",
+        subtitle: ""
+    },
+    Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00c6EB", "#005BEA"],
+        title: "",
+        subtitle: ""
+    },
+    Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: "",
+        subtitle: ""
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "",
+        subtitle: ""
+    },
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["89F7FE", "#66A6FF"],
+        title: "",
+        subtitle: ""
+    },
+    Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FFF253", "#FF7300"],
+        title: "clear",
+        subtitle: "you should go outside"
+    },
+    Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "",
+        subtitle: ""
+    },
+    Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "",
+        subtitle: ""
+    }
+}
 
 
-export default function Weather({temp}){
+export default function Weather({ temp, condition }) {
     return (
-        <View style={styles.container}>
-            <Text>{temp}</Text>
-        </View>
+        <LinearGradient
+            // Button Linear Gradient
+            colors={weatherOption[condition].gradient}
+            style={styles.container}
+        >
+            <StatusBar barStyle="light-content" />
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons
+                    size={96}
+                    name={weatherOption[condition].iconName}
+                    color="white"
+                />
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                    <Text style={styles.title}>{weatherOption[condition].title}</Text>
+                    <Text style={styles.subtitle}>{weatherOption[condition].subtitle}</Text>
+            </View>
+        </LinearGradient>
     );
 }
 
-Weather.PropTypes={
-    temp:PropTypes.number.isRequired,
-    condition: PropTypes.oneOf(["Thunderstorm", "Drizzle", "Rain", "Snow", "Clouds", "Atmosphere", "Clear","Mist", "haze","Dust","mist"]).isRequired
+Weather.propTypes = {
+    temp: propTypes.number.isRequired,
+    condition: propTypes.oneOf(["Thunderstorm",
+        "Drizzle", "Rain", "Snow", "Clouds", "Atmosphere", "Clear", "Mist", "Haze", "Dust",]).isRequired
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    temp: {
+        fontSize: 52,
+        color: "white"
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        color: "white",
+        fontSize: 44,
+        fontWeight: "300",
+        marginBottom: 10
+    },
+    subtitle: {
+        color: "white",
+        fontWeight: "600",
+        fontSize: 24
+    },
+    textContainer:{
+        paddingHorizontal:20,
+        alignItems: "flex-start"
     }
 });
